645330b54f99651fc6bbee29b46fb5f1c3f6ac8e
EstherCho-7 <dkgkqm777@gmail.com>
2024-07-17 10:44:10 +0900

    fixx

--- src/tot_cmd/cmd.py
+++ src/tot_cmd/cmd.py
@@ -1,7 +1,12 @@
 from tot_plus.plus import plus
 from tot_minus.minus import minus
+from tot_multi.multi import multi
 
-def call():
+def call_plus():
     plus()
+
+def call_minus():
     minus()
+    
+def call_multi():
     multi()
